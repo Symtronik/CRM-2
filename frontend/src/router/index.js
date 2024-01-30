c58f20c98b7119f56d8db6a80ffbe07fff1f6efc
Michal Kobialka <m.kobialka@ezze.pl>
2024-01-30 22:11:38 +0100

    Add views log in and dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import Dashboard from '../views/dashboard/Dashboard.vue'
+
 const routes = [
   {
     path: '/',
@@ -13,13 +16,23 @@ const routes = [
     component: SignUp
   },
   {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
 ]
 
 const router = createRouter({
