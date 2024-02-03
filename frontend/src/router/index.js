9f04c539949a09dbb0184eb7b9981ab1871e8151
Michal Kobialka <m.kobialka@ezze.pl>
2024-02-03 02:25:46 +0100

    Create page for showing the leads

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import Dashboard from '../views/dashboard/Dashboard.vue'
 import MyAccount from '../views/dashboard/MyAccount.vue'
+import Leads from '../views/dashboard/Leads.vue'
 
 const routes = [
   {
@@ -43,6 +44,14 @@ const routes = [
     path: '/dashboard/my-account',
     name: 'MyAccount',
     component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads',
+    name: 'Leads',
+    component: Leads,
     meta: {
       requireLogin: true
     }
